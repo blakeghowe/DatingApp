0de3a35dbc171a673a1a9d08b0f7634e11b67fc7
Blake Howe <blake.g.howe@gmail.com>
2019-06-18 22:15:21 -0400

    finished part 4

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  } 
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
