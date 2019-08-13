474c54ae65aaa846d74b417d6f9598ccc4db5e9d
Blake Howe <blake.g.howe@gmail.com>
2019-08-13 13:59:49 -0400

    added likes functionality

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -13,5 +13,28 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Sets primary key as combination of both ID's to prevent
+            // users from liking other users multiple times
+            builder.Entity<Like>()
+                .HasKey(k => new {k.LikerId, k.LikeeId});
+
+            // Create many-to-many relationship
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeId)
+                // Prevents a user from being deleted when a like is deleted
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
